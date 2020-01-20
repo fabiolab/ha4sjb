7de216617d73a54cfc2a7a133302b5f9f29bd631
Fabrice Depaulis <fabrice.depaulis@orange.com>
2020-01-20 17:07:47 +0100

    Add specific data to run on prod

--- yesterday_exporter.sh
+++ yesterday_exporter.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source .env
-OUTPUT="$(date -d 'yesterday 13:00' '+%Y-%m-%d')"
-python exporter.py "${OUTPUT}"
+cd /datas/vol2/w4a149500/home/w4a149500/ha4sjb
+source ./p3.6/bin/activate
+YESTERDAY="$(date -d 'yesterday 13:00' '+%Y-%m-%d')"
+python exporter.py "${YESTERDAY}"
