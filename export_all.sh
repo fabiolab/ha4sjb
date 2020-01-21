eaf0ccd544143d124cace75e71b5fa24600a40fb
Fabrice Depaulis <fabrice.depaulis@orange.com>
2020-01-21 10:33:47 +0100

    fix source .env

--- export_all.sh
+++ export_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source .env
 cd "${HA4SJB_HOME}"
+source .env
 source "${PYTHON_VENV_DIR}"/activate
 python exporter.py 2019-07-01
--- export_yesterday.sh
+++ export_yesterday.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source .env
 cd "${HA4SJB_HOME}"
+source .env
 source "${PYTHON_VENV_DIR}"/activate
 YESTERDAY="$(date -d 'yesterday 13:00' '+%Y-%m-%d')"
 python exporter.py "${YESTERDAY}"
