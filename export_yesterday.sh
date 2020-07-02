ecc2066b1e9681240d21c7ce1788fec0a4c7ea14
Fabrice Depaulis <fabrice.depaulis@orange.com>
2020-07-02 10:48:16 +0200

    Remove useless comments

--- export_yesterday.sh
+++ export_yesterday.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export HA4SJB_HOME=/datas/vol2/w4a149500/home/w4a149500/ha4sjb
+export HA4SJB_HOME=/home/kvjw3322/Nextcloud/Documents/Bad/ha4sjb
 cd "${HA4SJB_HOME}"
 source .env
 source "${PYTHON_VENV_DIR}"/activate
 YESTERDAY="$(date -d 'yesterday 13:00' '+%Y-%m-%d')"
-python exporter.py "${YESTERDAY}"
+python exporter.py --from_date="${YESTERDAY}"
