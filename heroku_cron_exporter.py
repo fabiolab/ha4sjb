cf202c94e4b401e0cf90d6aa1510a79d6b2c22f2
Fabrice Depaulis <fabrice.depaulis@orange.com>
2020-08-05 12:51:11 +0200

    change interval

--- heroku_cron_exporter.py
+++ heroku_cron_exporter.py
@@ -4,7 +4,7 @@ from exporter import ha2google
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', hours=24)
+@sched.scheduled_job('interval', days=1)
 def scheduled_job():
     ha2google()
 
