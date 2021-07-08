e0080b4c1e77d95396289bd94cf2e4b9f8fb8231
Fabrice Depaulis <fabrice.depaulis@orange.com>
2021-07-08 10:07:05 +0200

    Run import once at deploy

--- heroku_cron_exporter.py
+++ heroku_cron_exporter.py
@@ -1,3 +1,4 @@
+import pendulum
 from apscheduler.schedulers.blocking import BlockingScheduler
 from exporter import ha2google
 
@@ -8,5 +9,17 @@ sched = BlockingScheduler()
 def scheduled_job():
     ha2google()
 
+
+# Start import one minute before deploy
+one_minute_delay = pendulum.now().add(minutes=1)  # Let time for the server to start
+sched.add_job(
+    ha2google,
+    trigger="cron",
+    year=one_minute_delay.year,
+    month=one_minute_delay.month,
+    day=one_minute_delay.day,
+    hour=one_minute_delay.hour,
+    minute=one_minute_delay.minute,
+)
 
 sched.start()
